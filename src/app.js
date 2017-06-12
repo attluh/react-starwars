fffa99c524ef631bae666dc4cf35d8071083f5d4
Ash Reno <attluh@gmail.com>
2017-06-11 20:40:35 -0500

    Set up redux store

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(
+  applyMiddleware(thunk),
+));
 
 require('./index.html');
 
@@ -10,9 +19,9 @@ const container = document.querySelector('#app-container');
 
 // Render app
 ReactDOM.render(
-  <AppContainer>
+  <Provider store={store}>
     <App />
-  </AppContainer>
+  </Provider>
   , container
 );
 
